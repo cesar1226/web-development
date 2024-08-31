7d6d43beb5b3f3cc828dcb4f5c3bf104e5fb2343
Cesar <cesar_restrepo@hotmail.es>
2024-08-31 15:37:43 +0200

    renamed buttons

---
+++ config/config.js
@@ -0,0 +1,2 @@
+transactions_endpoint= 'https://7cng7ckhwj.execute-api.us-east-1.amazonaws.com/dev/transactions'
+balance_endpoint='https://7cng7ckhwj.execute-api.us-east-1.amazonaws.com/dev/accounts'
